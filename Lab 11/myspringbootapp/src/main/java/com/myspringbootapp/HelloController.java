e785f2b7cc3f781042bbbc5ab2703965dc2c511f
stefan1anuby <stefaneduard2002@gmail.com>
2023-05-17 09:34:45 +0300

    spring boot lab

---
+++ Lab 11/myspringbootapp/src/main/java/com/myspringbootapp/HelloController.java
@@ -0,0 +1,19 @@
+package com.myspringbootapp;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/hello/{name}")
+    String helloPerson(@PathVariable("name") String name) {
+        return "Hello " + name + " !";
+    }
+}
